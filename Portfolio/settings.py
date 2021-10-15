eaa495a5feedca2a9394e1a14b034089a8e08a38
Ihimekpen Osemudiamen Andrew <oihimekpen@gmail.com>
2021-10-15 02:07:08 +0100

    first

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -11,6 +11,26 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
+
+
+
+_USER=os.getenv("_USER")
+PASSWORD=os.getenv("PASSWORD")
+RECI=os.getenv("RECI")
+SKEY=os.getenv("SKEY")
+
+
+
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +40,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o+%2i^bxy@cq#2z3g%o#04_+tox*tzxc(c=ah3n2qisj44v++@'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dredgreat.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -37,6 +57,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #own
+    'Portfolioapp.apps.PortfolioappConfig'
 ]
 
 MIDDLEWARE = [
@@ -118,6 +140,25 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
+
+#email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER=env("_USER")
+EMAIL_HOST_PASSWORD=env("PASSWORD")
+EMAIL_PORT='587'
+# DEFAULT_FROM_EMAIL = 'osedevmail@gmail.com'
+
+
+
+
+
+MEDIA_ROOT=os.path.join(BASE_DIR,'static/images')
+MEDIA_URL = '/images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
---
+++ test.py
@@ -0,0 +1,6 @@
+s="james"
+d="love"
+def test():
+    return print(f"this is it : {s} , {d}")
+
+test()
