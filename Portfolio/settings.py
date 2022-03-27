f7208de112070267f4cc2b4dfb2ab345bcb32b50
ihimekpen osemudiamen <oihimekpen@gmail.com>
2022-03-27 08:57:12 +0100

    notification timeout

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -45,9 +45,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['dredgreat.herokuapp.com','127.0.0.1','dredgreat.com','www.dredgreat.com']
+ALLOWED_HOSTS = ['dredgreat.herokuapp.com','127.0.0.1','dredgreat.azurewebsites.net']
 
 
 # Application definition
@@ -154,7 +154,8 @@ STATICFILES_DIRS=[
 ]
 
 #email settings
-EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_HOST_USER=env("_USER")
